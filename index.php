ee29e2769434596a4b8bd740fd32861208ac33c1
Akash Prasher <prince.akash.prasher@gmail.com>
2022-03-08 13:10:02 +0530

    adding ui and styling for header and footer
    Signed-off-by: Akash Prasher <prince.akash.prasher@gmail.com>

--- index.php
+++ index.php
@@ -4,9 +4,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/style.css" />
     <title>Home - PHP Assignment</title>
 </head>
 <body>
-    
+<header>
+      <nav class="container">
+        <div class="navigation-bar">
+          <div><h3>php.</h3></div>
+          <div class="nav-links">
+            <ul>
+              <li><a class="active" href="./">Home</a></li>
+              <li><a href="./admin.php">Admin</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+    <main>
+      <div class="container">
+        <h1>lorem ipsum</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum
+          laboriosam aspernatur iure quo sint, aliquid quod dolorem quam quidem
+          earum delectus repudiandae molestiae modi quae deleniti eaque minus
+          esse natus?
+        </p>
+      </div>
+    </main>
+    <aside></aside>
+    <footer><p>php. &copy; 2021</p></footer>
 </body>
 </html>
