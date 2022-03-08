8b1eb8146a98a75fdf0f14eccbc0cd6e68610472
Akash Prasher <prince.akash.prasher@gmail.com>
2022-03-08 13:30:18 +0530

    complete static ui
    Signed-off-by: Akash Prasher <prince.akash.prasher@gmail.com>

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="./assets/css/style.css" />
     <title>Home - PHP Assignment</title>
-</head>
-<body>
-<header>
+  </head>
+  <body>
+    <header>
       <nav class="container">
         <div class="navigation-bar">
           <div><h3>php.</h3></div>
@@ -24,6 +24,7 @@
     <main>
       <div class="container">
         <h1>lorem ipsum</h1>
+        <hr />
         <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum
           laboriosam aspernatur iure quo sint, aliquid quod dolorem quam quidem
@@ -32,7 +33,27 @@
         </p>
       </div>
     </main>
-    <aside></aside>
+    <aside>
+      <div class="container">
+        <div class="aside-content">
+          <img
+            class="aside-img"
+            src="./assets/images/img1.jpg"
+            alt="Edvard Munch, 1893, The Scream"
+          />
+          <div class="aside-con">
+            <h3>Image Description</h3>
+            <hr />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+              maiores, inventore veritatis exercitationem dolorum commodi magni
+              explicabo repudiandae eligendi harum culpa iste quos sed. Sed et
+              in quos mollitia impedit!
+            </p>
+          </div>
+        </div>
+      </div>
+    </aside>
     <footer><p>php. &copy; 2021</p></footer>
-</body>
+  </body>
 </html>
